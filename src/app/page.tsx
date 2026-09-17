df2849429a1c46718af81cd3a933c53b6f3556a6
Victoriano012 <victorconchello@gmail.com>
2026-09-16 21:44:41 -0400

    Raise the sign-out icon further so it sits level with the top of 'ToDo'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default async function Home() {
               strokeLinecap="round"
               strokeLinejoin="round"
               aria-hidden="true"
-              className="relative -top-px"
+              className="relative -top-[3px]"
             >
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
               <polyline points="16 17 21 12 16 7" />
